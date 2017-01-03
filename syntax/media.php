2c97802d8e9fca0cc9c75966fcfcfcd7d672c622
Satoshi Sahara <sahara.satoshi@gmail.com>
2017-01-03 09:07:57 +0900

    supersede match pattern for media

---
+++ syntax/media.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * DokuWiki Plugin HyperLink; Syntax media
+ *
+ * @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author  Satoshi Sahara <sahara.satoshi@gmail.com>
+ *
+ * Supersede DokuWiki's connect patterns for media
+ *
+ */
+
+// must be run within Dokuwiki
+if (!defined('DOKU_INC')) die();
+
+class syntax_plugin_hyperlink_media extends DokuWiki_Syntax_Plugin {
+
+    protected $mode;
+    protected $pattrens;
+
+    function __construct() {
+        $this->mode = substr(get_class($this), 7);
+
+        // @see class Doku_Parser_Mode_media in inc/parser/parser.php
+        // ignore single "}" and check "}}"
+        //$this->patterns[] = "\{\{[^\}]+\}\}";
+        //$this->patterns[] = '\{\{.+?\}\}(?!\})';
+        $this->patterns[] = '\{\{.+?\}\}';
+    }
+
+    function getType()  { return 'substition'; }
+    function getPType() { return 'normal'; }
+    function getSort()  { return 319; } // < Doku_Parser_Mode_media(=320)
+
+    /**
+     * Connect pattern to lexer
+     */
+    function connectTo($mode) {
+        foreach ( $this->patterns as $pattern ) {
+            $this->Lexer->addSpecialPattern($pattern, $mode, $this->mode);
+        }
+    }
+
+    /**
+     * handle syntax
+     */
+    function handle($match, $state, $pos, Doku_Handler $handler) {
+
+        $handler->media($match, $state, $pos);
+        return false; // do not call $this->render()
+    }
+
+    /**
+     * Create output
+     */
+    function render($format, Doku_Renderer $renderer, $data) {
+        return true;
+    }
+
+}
